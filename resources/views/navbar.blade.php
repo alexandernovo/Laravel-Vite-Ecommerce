f5d21b8539e724800c52cb1d3106dc64b5132626
Alexander <alexandernovo84@gmail.com>
2023-09-15 17:23:50 +0800

    change route

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -25,7 +25,7 @@
                     </svg>
                 </button>
                 <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
-                    <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <ul class="flex items-center flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
                             <a href="{{route('home')}}" class="block py-2 pl-3 pr-4 {{Route::is('home')?'text-blue-700':'text-white'}} bg-blue-700 rounded md:bg-transparent md:p-0 dark:bg-blue-600 md:dark:bg-transparent hover:text-blue-500">Home</a>
                         </li>
@@ -38,20 +38,30 @@
                             <a href="{{route('products')}}" class="block py-2 pl-3 {{Route::is('products')?'text-blue-700':'text-white'}} pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Products</a>
                         </li>
                         <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                        </li>
+                        <li class="relative">
                             <button id="menu-button" data-dropdown-toggle="dropdownNavbar" class="{{Route::is('login')||Route::is('register')?'text-blue-700':'text-white'}} flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
+                                @if (!auth()->check())
                                 Get Started
+                                @else
+                                <div class="w-8 h-8  rounded-full overflow-hidden">
+                                    <img class="w-full h-full object-cover" src="assets/images/placeholder.jpg">
+                                </div>
+                                @endif
                                 <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
-                                </svg></button>
+                                </svg>
+                            </button>
                             <!-- Dropdown menu -->
-                            <div id="menu-content" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600 absolute">
+                            <div id="menu-content" class="z-10 hidden font-normal mt-1 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600 absolute right-0">
                                 @if (auth()->check())
                                 <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
                                     <li>
                                         <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
                                     </li>
                                     <li>
-                                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Profile</a>
                                     </li>
                                     <li>
                                         <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
@@ -74,15 +84,14 @@
                                         Sign up
                                     </a>
                                     @else
-                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Sign out</a>
+                                    <form method="POST" action="{{route('logout')}}" role="none">
+                                        @csrf
+                                        <button type="submit" class="block px-4 py-2 w-full text-start text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Sign out</button>
+                                    </form>
                                     @endif
                                 </div>
                             </div>
                         </li>
-                        <li>
-                            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-                        </li>
-
                     </ul>
                 </div>
             </div>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -11,9 +11,6 @@
                 <div class="w-3/4 mx-auto">
                     <h1 class="text-black text-3xl font-bold mb-4">Sign up</h1>
                     <div class="mb-3">
-                        @if(session()->has('success'))
-                        <p class="p-3 bg-green-600 text-white rounded-lg mb-2 text-[14px]">{{ session('success') }}</p>
-                        @endif
                         <div class="relative">
                             <input name="name" type="text" id="floating_outlined" value="{{old('name')}}" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 {{$errors->has('name')?'border border-red-300':'border'}} appearance-none text-gray-500 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                             <label for="floating_outlined" class="absolute text-sm text-gray-500 {{$errors->has('name')?'text-red-500':'text-gray-400'}} duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
