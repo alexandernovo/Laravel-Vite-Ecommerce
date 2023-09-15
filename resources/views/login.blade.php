a67c147410c96067288b71b0287017c1ac23c5c5
Alexander <alexandernovo84@gmail.com>
2023-09-15 14:42:26 +0800

    Fix Designs

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,30 +2,37 @@
 @section('content')
 <form method="post" action="{{route('login')}}">
     @csrf
-    <div class="h-screen container flex mx-auto justify-between">
+    <div class="container flex mx-auto justify-between items-center mt-12">
         <div class="w-full sm:w-full md:w-full lg:w-1/2 p-10">
-            <img class="w-full h-full" src="assets/images/shoes-transparent.png">
+            <img class="w-full h-full" src="assets/images/shoes.jpg">
         </div>
         <div class="w-full sm:w-full md:w-full lg:w-1/2 p-12 flex items-center">
             <div class="flex-1 ">
                 <div class="w-3/4 mx-auto">
+                    <h1 class="text-black text-lg font-bold mb-2">Welcome Back!</h1>
                     <h1 class="text-black text-3xl font-bold mb-4">Sign in to your Account</h1>
-                    <div class="mb-2">
-                        <label for="username" class="block text-sm text-gray-500 font-semibold mb-1">Username</label>
-                        <input name="username" class="appearance-none w-full focus:outline-none focus:shadow-outline focus:border-purple-400 text-gray-500 leading-tight py-2 px-3 border rounded-lg" id="username" />
+                    <div class="relative mb-3">
+                        <input name="username" type="text" id="floating_outlined" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border appearance-none dark:text-white dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                        <label for="floating_outlined" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
+                            Username
+                        </label>
                     </div>
                     <div class="mb-3">
-                        <label for="username" class="block text-sm text-gray-500 font-semibold mb-1">Password</label>
-                        <input name="password" class="appearance-none w-full focus:outline-none focus:shadow-outline focus:border-purple-400 text-gray-500 leading-tight py-2 px-3 border rounded-lg" id="username" />
+                        <div class="relative mb-2">
+                            <input name="password" type="text" id="floating_outlined" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border appearance-none dark:text-white dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                            <label for="floating_outlined" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
+                                Password
+                            </label>
+                        </div>
                         @if ($errors->has('error'))
-                        <p class="text-red-500 text-sm m-0">
+                        <p class="text-red-500 text-sm m-0 text-[13px]">
                             {{ $errors->first('error') }}
                         </p>
                         @endif
                     </div>
                     <div class="mb-2">
-                        <button type="submit" class="bg-blue-500 hover:bg-blue-500 w-full text-white font-bold py-2 px-4 rounded-lg">
-                            Sign in
+                        <button type="submit" class="bg-blue-500 hover:bg-blue-500 w-full text-white py-2.5 px-4 rounded-lg">
+                            SIGN IN
                         </button>
                         <p class="text-decoration-none text-center mt-3 text-sm">No Account? <a href="{{route('register')}}" class="text-blue-400">Sign up here</a></p>
                     </div>
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -10,93 +10,86 @@
 
 <body>
     <header class="sticky top-0">
-        <nav class="bg-white sticky top-0 flex justify-center z-10">
-            <div class="container flex justify-between p-5 items-center">
-                <div class="flex items-center">
-                    <div class="w-10 h-10">
+        <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="#" class="flex items-center">
+                    <div class="w-10 h-10 shadow">
                         <img class="w-full h-full" src="assets/images/logo.png" />
                     </div>
-                    <span>
-                        <h4 class="font-bold text-lg">SHOES</h4>
-                    </span>
-                </div>
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">AEN</span>
+                </a>
+                <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                    </svg>
+                </button>
+                <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+                    <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                            <a href="{{route('home')}}" class="block py-2 pl-3 pr-4 {{Route::is('home')?'text-blue-700':'text-white'}} bg-blue-700 rounded md:bg-transparent md:p-0 dark:bg-blue-600 md:dark:bg-transparent hover:text-blue-500">Home</a>
+                        </li>
 
-                @if (auth()->check())
-                @if (Auth::user()->role == 1)
-                <!-- Admin Navbar -->
-                <ul class="flex justify-between">
-                    <li><a class="px-3 px-2 font-semibold {{Route::is('home')?'text-black':'text-gray-600'}} text-sm mx-3" href="{{route('home')}}">HOME</a></li>
-                    <li><a class="px-3 px-2 font-semibold {{Route::is('products')?'text-black':'text-gray-600'}} text-sm mx-3" href="{{route('products')}}">PRODUCTS</a></li>
-                    <li><a class="px-3 px-2 font-semibold text-gray-600 text-sm mx-3" href="#">GRAPHS</a></li>
-                    <li><a class="px-3 px-2 font-semibold text-gray-600 text-sm mx-3" href="#">TRANSACTIONS</a></li>
-                </ul>
-                @else
-                <!-- User Navbar -->
-                <ul class="flex justify-between">
-                    <li><a class="px-3 px-2 font-semibold {{Route::is('home')?'text-black':'text-gray-600'}} text-sm mx-3" href="{{route('home')}}">HOME</a></li>
-                    <li><a class="px-3 px-2 font-semibold {{Route::is('products')?'text-black':'text-gray-600'}} text-sm mx-3" href="{{route('products')}}">PRODUCTS</a></li>
-                    <li><a class="px-3 px-2 font-semibold {{Route::is('shop')?'text-black':'text-gray-600'}} text-sm mx-3" href="{{route('shop')}}">SHOP</a></li>
-                    <li><a class="px-3 px-2 font-semibold text-gray-600 text-sm mx-3" href="#">TRANSACTIONS</a></li>
-                </ul>
-                @endif
-                @else
-                <ul class="flex justify-between">
-                    <li><a class="px-3 px-2 font-semibold {{Route::is('home')?'text-black':'text-gray-600'}} text-sm mx-3" href="{{route('home')}}">HOME</a></li>
-                    <li><a class="px-3 px-2 font-semibold text-gray-600 text-sm mx-3" href="#">ABOUT</a></li>
-                    <li><a class="px-3 px-2 font-semibold {{Route::is('products')?'text-black':'text-gray-600'}} text-sm mx-3" href="{{route('products')}}">PRODUCTS</a></li>
-                    <li><a class="px-3 px-2 font-semibold text-gray-600 text-sm mx-3" href="#">SUPPORT</a></li>
-                </ul>
-                @endif
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+                        </li>
 
-                <div class="flex">
-                    @if (auth()->check())
-                    <div class="w-6 h-6 me-6">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-                            <path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z" />
-                        </svg>
-                    </div>
-                    @endif
+                        <li>
+                            <a href="{{route('products')}}" class="block py-2 pl-3 {{Route::is('products')?'text-blue-700':'text-white'}} pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Products</a>
+                        </li>
+                        <li>
+                            <button id="menu-button" data-dropdown-toggle="dropdownNavbar" class="{{Route::is('login')||Route::is('register')?'text-blue-700':'text-white'}} flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
+                                Get Started
+                                <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
+                                </svg></button>
+                            <!-- Dropdown menu -->
+                            <div id="menu-content" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600 absolute">
+                                @if (auth()->check())
+                                <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                                    </li>
+                                </ul>
+                                @endif
 
-                    <div class="w-6 h-6">
-                        <!-- <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-                            <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
-                        </svg> -->
-                        @if (!auth()->check())
-                        @if(!Route::is('login'))
-                        <a class="py-2 px-1 rounded font-semibold text-gray-600 text-sm hover:bg-blue-500 hover:text-white" href="{{route('login')}}">LOGIN</a>
-                        @else
-                        <a class="py-2 px-1 rounded font-semibold text-gray-600 text-sm hover:bg-blue-500 hover:text-white" href="{{route('register')}}">REGISTER</a>
-                        @endif
-                        @else
-                        <div class="relative inline-block text-left">
-                            <div>
-                                <button type="button" class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white text-sm font-semibold text-gray-900" id="menu-button" aria-expanded="true" aria-haspopup="true">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" />
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                    </svg>
-                                </button>
-                            </div>
-                            <div class="absolute right-0 z-10 mt-2 w-56 hidden origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" id="menu-content" aria-labelledby="menu-button" tabindex="-1">
-                                <div class="py-1" role="none">
-                                    <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Account settings</a>
-                                    <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Support</a>
-                                    <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">License</a>
-                                    <form method="POST" action="{{route('logout')}}" role="none">
-                                        @csrf
-                                        <button type="submit" class="text-gray-700 block w-full px-4 py-2 text-left text-sm" role="menuitem" tabindex="-1" id="menu-item-3">Sign out</button>
-                                    </form>
+                                <div class="py-1">
+                                    @if (!auth()->check())
+                                    <a href="{{route('login')}}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white flex">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 me-1">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+                                        </svg>
+                                        Sign in
+                                    </a>
+                                    <a href="{{route('register')}}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white flex">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 me-1">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
+                                        </svg>
+                                        Sign up
+                                    </a>
+                                    @else
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Sign out</a>
+                                    @endif
                                 </div>
                             </div>
-                        </div>
-                        @endif
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                        </li>
 
-                    </div>
+                    </ul>
                 </div>
             </div>
         </nav>
+
     </header>
-    <main>
+    <main class="h-full">
         @yield('content')
     </main>
     @vite('resources/js/app.js')
